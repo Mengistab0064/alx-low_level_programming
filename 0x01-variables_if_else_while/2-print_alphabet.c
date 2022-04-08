48a2f23b52654a25a1ab5c0486acac08a42d8ce3
Mengistab <mengistab.shiferaw@gmail.com>
2022-04-08 13:02:04 -0700

    task2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: Print the lowercase alphabet
+ * Return: 0
+ */
+int main(void)
+{
+int c;
+for (c = 0; c < 26; ++c)
+putchar('a' + c);
+
+putchar('\n');
+
+return (0);
+}
