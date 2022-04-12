9f0ba12026e56f9506105607acbaadb1db411c2a
Mengistab <mengistab.shiferaw@gmail.com>
2022-04-12 12:53:24 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Check description
+ * Description: It prints the alphabet in lowercase, followed by a new line
+ * Return: Nothing.
+ */
+void print_alphabet(void)
+{
+	char letter;
+
+	for (letter = 'a'; letter <= 'z'; letter++)
+		_putchar(letter);
+	_putchar('\n');
+}
+© 2022 GitHub, Inc.
+Terms
+
